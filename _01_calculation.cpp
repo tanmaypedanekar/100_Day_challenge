ee3d19bd101e7339c6eceebffd1beae472ab28cc
tanmaypedanekar <tanmaypedanekar4528@gmail.com>
2026-01-13 19:58:50 +0530

    Adding calc File

---
+++ _01_calculation.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,add=0,mul=0,sub=0,div=0;
+    cin>>a>>b;
+    add=a+b;
+    mul=a*b;
+    sub=a-b;
+    div=a/b;
+    cout<<add<<endl<<sub<<endl<<mul<<endl<<div;
+    return 0;
+}
